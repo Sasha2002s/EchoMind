a2df1ad05c45f306412c6e0570a6617cfc5934ca
Sasha <alex2017stepanov@gmail.com>
2026-02-26 08:23:54 +0100

    Initial Commit

---
+++ EchoMind/EchoMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoMindApp.swift
+//  EchoMind
+//
+//  Created by Oleksandr Stepanov on 26.02.26.
+//
+
+import SwiftUI
+
+@main
+struct EchoMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
